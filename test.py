b7a0c436c60662c5883e49277ab594e4933d0f51
mouadkahouadji <115421978+mouadkahouadji@users.noreply.github.com>
2023-06-07 11:31:50 +0200

    Create test.py
    show my github cours

---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd 
+import numbers
+
+def my_function(x,y):
+    s=x+y
+    return print(s)
+my_function(3,2)
+
+
+import matplotlib.pyplot as plt 
+
+
+for i in range(100):
+    j=i+10
+plt.scatter(i,j)
+plt.show()
